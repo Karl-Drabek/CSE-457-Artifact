47165cd54ec919d21520beb71966963ea8613711
aidanjc19 <aidan.jc19@gmail.com>
2026-05-26 17:14:58 -0700

    Ship building mechanics improvements

--- Assets/Scripts/MoveableObject.cs
+++ Assets/Scripts/MoveableObject.cs
@@ -5,11 +5,21 @@ public class MoveableObject : MonoBehaviour
     private bool selected;
     private Vector3 startingPos;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private float opacity;
+
+    [SerializeField]
+    [Range(0f, 1f)]
+    float selected_opacity = 0.8f;
+    const float DEFAULT_OPACITY = 0f;
+    Renderer renderer;
+
+
+    void Awake()
     {
         selected = false;
         startingPos = transform.position;
+        opacity = 0f;
+        renderer = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
@@ -20,19 +30,57 @@ public class MoveableObject : MonoBehaviour
             RaycastHit rayHit;
             if (ShipBuildController.CameraToMouseRay(out rayHit))
             {
-                transform.position = rayHit.point + Vector3.up * transform.localScale.y;
+                Reorient(rayHit);
             }
         }
     }
 
     public void SetSelected() 
     {
-        selected = !selected;
-        gameObject.layer = selected ? 2 : 0;
+        selected = true;
+        gameObject.layer = 2;
+        SetOpacity(selected_opacity);
+    }
+
+    public void SetDeselected()
+    {
+        selected = false;
+        SetOpacity(DEFAULT_OPACITY);
+        gameObject.layer = 0;
     }
 
     public Vector3 GetPosition()
     {
         return transform.position;
+    }
+
+    public void Reorient(RaycastHit rayHit)
+    {
+        Vector3 norm = rayHit.normal.normalized;
+        gameObject.transform.up = norm;
+        transform.rotation = Quaternion.FromToRotation(Vector3.up, norm);
+        transform.position = rayHit.point + norm * transform.localScale.y;
+    }
+
+    void SetOpacity(float opacity)
+    {
+        Material[] materials = renderer.materials;
+        for (int j = 0; j < materials.Length; j++)
+        {
+            Material mat = materials[j];
+            if (opacity > 0f)
+            {
+                ShipBuildController.SetMaterialTransparent(mat);
+                
+            }
+            else
+            {
+                ShipBuildController.SetMaterialOpaque(mat);
+            }
+            Color color = mat.color;
+            color.a = opacity;
+            mat.color = color;
+        }
+        renderer.materials = materials;
     }
 }
--- Assets/Scripts/ShipBuildController.cs
+++ Assets/Scripts/ShipBuildController.cs
@@ -41,7 +41,7 @@ public class ShipBuildController : MonoBehaviour
         {
             return;
         }
-        if (rightClick.WasPressedThisFrame())
+        if (rightClick.WasPressedThisFrame() && moveable == null)
         {
             RaycastHit RayHit;
             if (CameraToMouseRay(out RayHit))
@@ -54,9 +54,14 @@ public class ShipBuildController : MonoBehaviour
                 moveable.SetSelected();
             }
         }
+        else if (rightClick.WasPressedThisFrame() && moveable != null) 
+        {
+            Destroy(moveable.gameObject);
+            moveable = null;
+        }
         else if (rightClick.WasReleasedThisFrame() && moveable != null)
         {
-            moveable.SetSelected();
+            moveable.SetDeselected();
             moveable = null;
         }
         else if (leftClick.WasPressedThisFrame() && moveable == null)
@@ -67,7 +72,7 @@ public class ShipBuildController : MonoBehaviour
                 GameObject target = RayHit.transform.gameObject;
                 Vector3 pos = RayHit.point;
                 GameObject new_object;
-                if (target != null) 
+                if (target != null)
                 {
                     pos = pos + Vector3.up * target.transform.localScale.y;
                     new_object = Instantiate(obj, pos, Quaternion.identity);
@@ -78,9 +83,7 @@ public class ShipBuildController : MonoBehaviour
         }
         else if (leftClick.WasPressedThisFrame() && moveable != null)
         {
-            Instantiate(obj, moveable.GetPosition(), Quaternion.identity);
-            moveable.SetSelected();
-            moveable = null;
+            Instantiate(obj, moveable.GetPosition(), moveable.gameObject.transform.rotation);
         }
     }
 
@@ -88,8 +91,35 @@ public class ShipBuildController : MonoBehaviour
     {
         Ray ray = Instance.mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
         return Physics.Raycast(ray, out RayHit);
-        //return false;
+    }
 
+
+    // For URP Lit shader, other shaders will have different property names
+    public static void SetMaterialTransparent(Material mat)
+    {
+        mat.SetFloat("_Surface", 1f);
+        mat.SetFloat("_Blend", 0f);
+        mat.SetFloat("_SrcBlend", (float)UnityEngine.Rendering.BlendMode.SrcAlpha);
+        mat.SetFloat("_DstBlend", (float)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+        mat.SetFloat("_ZWrite", 0f);
+        mat.EnableKeyword("_SURFACE_TYPE_TRANSPARENT");
+        mat.DisableKeyword("_ALPHATEST_ON");
+        mat.DisableKeyword("_ALPHABLEND_ON");
+        mat.EnableKeyword("_ALPHAPREMULTIPLY_ON");
+        mat.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Transparent;
+    }
+
+    public static void SetMaterialOpaque(Material mat)
+    {
+        mat.SetFloat("_Surface", 0f);
+        mat.SetFloat("_SrcBlend", (float)UnityEngine.Rendering.BlendMode.One);
+        mat.SetFloat("_DstBlend", (float)UnityEngine.Rendering.BlendMode.Zero);
+        mat.SetFloat("_ZWrite", 1f);
+        mat.DisableKeyword("_SURFACE_TYPE_TRANSPARENT");
+        mat.DisableKeyword("_ALPHATEST_ON");
+        mat.DisableKeyword("_ALPHABLEND_ON");
+        mat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+        mat.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Geometry;
     }
 
 
